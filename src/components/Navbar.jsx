4059756bc1a25574aae0d5a215bd024a716edf2a
NaqeebH <Naqeeb.1011@gmail.com>
2025-12-08 20:29:46 +0100

    WIP: temporary changes to Navbar before switching branches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ function Navbar() {
     <header className="py-6 border-b border-gray-300 font-serif">
       <div className="max-w-5xl mx-auto px-10 grid grid-cols-3 items-center">
         
-        <h1 className="text-4xl font-serif text-center col-start-2">
+        <h1 className="text-black text-4xl font-serif text-center col-start-2">
           Event Scheduler
         </h1>
 
         <nav className="space-x-6 justify-self-end">
           <a href="#" className="text-gray-700 hover:text-black">Sign In</a>
-          <a href="#" className="text-gray-700 hover:text-black">Sign Up</a>
+          <a href="#" className="text-gray-800 hover:text-black">Sign Up</a>
         </nav>
       </div>
     </header>
