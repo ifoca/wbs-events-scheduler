5abafdab8063bc1a6b6c1d20abcd0f545091a5c7
NaqeebH <Naqeeb.1011@gmail.com>
2025-12-08 21:16:29 +0100

    Fetch events from API, sort chronologically, and render EventCards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from "react";
 import EventsList from '../components/EventsList';
+import EventCard from "../components/EventCard";
 
 function Home() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/events")
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the events array from data.results
+        const sortedEvents = data.results.sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        );
+
+        setEvents(sortedEvents);
+      })
+      .catch((error) => {
+        console.error("Error fetching events:", error);
+      });
+  }, []);
+
   return (
-    <div className="m-8">
-      <p>Home page</p>
-      <EventsList />
+    <div className="flex flex-col gap-4 p-4">
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
+      ))}
     </div>
   );
 }
 
 export default Home;
-
