676dcfa9d159baecea32142b4ddbe0f317e084d2
ifoca <foca.ionut@gmail.com>
2025-12-11 19:24:45 +0100

    use load and erro context on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import EventsList from '../components/EventsList';
-import EventCard from "../components/EventCard";
+import LoadingMessage from '../components/LoadingMessage';
+import ErrorMessage from '../components/ErrorMessage';
+import useGeneralStates from '../contexts/GeneralContext';
 
 function Home() {
   const [events, setEvents] = useState([]);
+  const { loading, setLoading, error, setError } = useGeneralStates();
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/events")
+    setLoading(true);
+    setError('');
+
+    fetch('http://localhost:3001/api/events')
       .then((response) => response.json())
       .then((data) => {
         // Extract the events array from data.results
-        const sortedEvents = data.results.sort(
-          (a, b) => new Date(a.date) - new Date(b.date)
-        );
-
+        const sortedEvents = data.results.sort((a, b) => new Date(a.date) - new Date(b.date));
         setEvents(sortedEvents);
       })
       .catch((error) => {
-        console.error("Error fetching events:", error);
+        console.error('Error fetching events:', error);
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
-  console.log("EVENT OBJECT:", events[0]);
-  
+  console.log('EVENT OBJECT:', events[0]);
+
+  if (loading) {
+    return <LoadingMessage />;
+  }
+
+  if (error) {
+    return <ErrorMessage error={error} />;
+  }
 
   return (
     <div className="p-8">
